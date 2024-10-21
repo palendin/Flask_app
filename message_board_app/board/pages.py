ca14b14e0a19938766a806a952a5129c0f86673c
Wayne Wu <waynechemicalengineer@gmail.com>
2024-10-21 15:58:35 -0700

    new version

--- message_board_app/board/pages.py
+++ message_board_app/board/pages.py
@@ -1,15 +1,15 @@
 # As your Flask project grows, you have lots of view functions and other related bits of code that work together. 
 # Instead of adding all these directly to a basic app.py file, you can create blueprints that you register in your application factory
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 # blueprints are existing view functions
-bp = Blueprint("pages", __name__)
+bp = Blueprint('pages', __name__)
 
-# using the view functions to the route
+# using the view functions to define routes
 @bp.route("/")
 def home():
-    return "Hello, Home!"
+    return render_template("/home.html") #looks for the html files in /board/templates
 
 @bp.route("/about")
 def about():
-    return "Hello, About!"
+    return render_template("/about.html")
